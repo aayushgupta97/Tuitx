16e2747ff11673a8d3e77c84873116c1e5cc8add
aayushgupta97 <aayushgupta2097@gmail.com>
2018-07-21 14:26:43 +0530

    Adding TuitX

---
+++ TuitX/config.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * The base MySQL settings of Osclass
+ */
+define('MULTISITE', 0);
+
+/** MySQL database name for Osclass */
+define('DB_NAME', 'c9');
+
+/** MySQL database username */
+define('DB_USER', 'root');
+
+/** MySQL database password */
+define('DB_PASSWORD', '');
+
+/** MySQL hostname */
+define('DB_HOST', 'localhost');
+
+/** Database Table prefix */
+define('DB_TABLE_PREFIX', 'oc_');
+
+define('REL_WEB_URL', '/');
+
+define('WEB_PATH', 'https://tuitx-aayushgupta.c9users.io/');
